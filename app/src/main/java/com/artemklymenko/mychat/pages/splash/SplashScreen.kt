87ac372ee6559f74d2c93b92875c85c27a5df6a4
Artem <artemklymenko892@gmail.com>
2024-10-24 18:05:21 +0300

    Created SplashScreen

---
+++ app/src/main/java/com/artemklymenko/mychat/pages/splash/SplashScreen.kt
@@ -0,0 +1,66 @@
+package com.artemklymenko.mychat.pages.splash
+
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.material3.CircularProgressIndicator
+import androidx.compose.material3.Scaffold
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import com.artemklymenko.mychat.R
+import com.artemklymenko.mychat.ui.theme.MyChatTheme
+
+@Composable
+fun SplashScreen(
+    modifier: Modifier,
+    onNavigateToSignIn: () -> Unit
+) {
+    Scaffold(
+        modifier = modifier
+    ) {
+        Column(
+            modifier = modifier
+                .fillMaxSize()
+                .padding(it),
+            horizontalAlignment = Alignment.CenterHorizontally,
+            verticalArrangement = Arrangement.Center
+        ) {
+            Image(
+                modifier = modifier.size(128.dp),
+                painter = painterResource(id = R.drawable.message_image),
+                contentDescription = null
+            )
+            Spacer(modifier = modifier.height(16.dp))
+            CircularProgressIndicator()
+            LaunchedEffect(key1 = Unit) {
+                onNavigateToSignIn()
+            }
+        }
+    }
+}
+
+@Preview
+@Composable
+private fun SplashScreenPreviewLight(){
+    MyChatTheme {
+        SplashScreen(modifier = Modifier, onNavigateToSignIn = {})
+    }
+}
+
+@Preview
+@Composable
+private fun SplashScreenPreviewDark(){
+    MyChatTheme(darkTheme = true) {
+        SplashScreen(modifier = Modifier, onNavigateToSignIn = {})
+    }
+}
