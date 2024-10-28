fbf87c58aa5839d2ff0dc88da17ea5b139ab06c3
Artem <artemklymenko892@gmail.com>
2024-10-28 19:41:19 +0200

    Created ValidateRegisterInputUseCase

---
+++ app/src/main/java/com/artemklymenko/mychat/domain/use_case/ValidateRegisterInputUseCase.kt
@@ -0,0 +1,48 @@
+package com.artemklymenko.mychat.domain.use_case
+
+import com.artemklymenko.mychat.domain.model.RegisterInputValidationType
+import com.artemklymenko.mychat.utils.StringExtension
+
+class ValidateRegisterInputUseCase {
+
+    operator fun invoke(
+        email: String,
+        password: String,
+        passwordRepeated: String
+    ): RegisterInputValidationType {
+
+        if(email.isEmpty() || password.isEmpty() || passwordRepeated.isEmpty()){
+            return RegisterInputValidationType.EmptyField
+        }
+
+        if(!StringExtension().checkEmail(email)){
+            return RegisterInputValidationType.NoEmail
+        }
+
+        if(password != passwordRepeated){
+            return RegisterInputValidationType.PasswordsDoNotMatch
+        }
+
+        if(password.length < 8){
+            return RegisterInputValidationType.PasswordIsTooShort
+        }
+
+        if(email.length < 14){
+            return RegisterInputValidationType.EmailIsTooShort
+        }
+
+        if(!StringExtension().containsSpecialChar(password)){
+            return RegisterInputValidationType.PasswordSpecialCharMissing
+        }
+
+        if(!StringExtension().containsNumber(password)){
+            return RegisterInputValidationType.PasswordNumberMissing
+        }
+
+        if(!StringExtension().containsUpperCase(password)){
+            return RegisterInputValidationType.PasswordUpperCaseMissing
+        }
+
+        return RegisterInputValidationType.Valid
+    }
+}
